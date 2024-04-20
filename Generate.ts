1673f397c7ec5cd900382fdfdab4fe189e325c0f
Siddharth Sridharan <sidsri513@gmail.com>
2024-04-20 22:16:40 +1000

    Initial Commit

---
+++ Generate.ts
@@ -0,0 +1,22 @@
+interface User {
+    id: string,
+    name: string,
+    email: string,
+    phone: string,
+    portDescription: string,
+    googleToken: string,
+};
+
+interface Address {
+    id: string,
+    userId: string,
+    state: string,
+    city: string,
+    postcode: string,
+};
+
+interface Education {
+    id: string,
+    
+};
+ 
